52867953d2c653c044c583e88e6e368cf5f98419
Arata <aratadayoon@yahoo.co.jp>
2024-04-18 19:37:41 +0900

    change design

--- ReactPortfolio/email.php
+++ ReactPortfolio/email.php
@@ -29,10 +29,11 @@ if (!empty($postData)) {
     $headers .= 'From: ' . 'RealPortfolio' . "\r\n";
 
     $message = "<p>以下の内容でお問い合わせを承りました。</p>";
-    $message .= "<p>氏名: " . $postData['sei'] . "</p>";
-    $message .= "<p>題名: " . $postData['mei'] . "</p>";
-    $message .= "<p>メールアドレス: " . $postData['email'] . "</p>";
-    $message .= "<p>お問い合わせ内容: " . $postData['message'] . "</p>";
+    $message = "<p>数日以内にご連絡いたしますので、しばらくお待ちください。</p>";
+    $message .= "<p style='padding: 12px; border-left: 4px solid #d0d0d0; font-style: italic;'>氏名: " . $postData['sei'] . "</p>";
+    $message .= "<p style='padding: 12px; border-left: 4px solid #d0d0d0; font-style: italic;'>題名: " . $postData['mei'] . "</p>";
+    $message .= "<p style='padding: 12px; border-left: 4px solid #d0d0d0; font-style: italic;'>メールアドレス: " . $postData['email'] . "</p>";
+    $message .= "<p style='padding: 12px; border-left: 4px solid #d0d0d0; font-style: italic;'>お問い合わせ内容: " . $postData['message'] . "</p>";
 
     if(mail($to, $subject, $message, $headers)){
         echo json_encode(array("status" => "success"));
